151801882a5a1bcc8467d7c978694621fa6fc9f4
JeremyRH <holcomb227@gmail.com>
2021-05-03 00:22:59 -0700

    Generate test node_modules dir

--- scripts/test.js
+++ scripts/test.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 const { exec } = require('./exec');
+const packageJson = require('../package.json');
 
-const distDir = path.resolve(__dirname, '../tests/dist');
-const tsconfig = path.resolve(__dirname, '../tests/tsconfig.json');
+const rootDir = path.resolve(__dirname, '..');
+const testDistDir = path.join(rootDir, 'tests/dist');
+const tsconfig = path.join(rootDir, 'tests/tsconfig.json');
+const testNodeModulesDir = path.join(rootDir, 'tests/node_modules');
 
-fs.promises
-	.rmdir(distDir, { recursive: true })
-	.then(() => {
-		return exec(`tsc --project ${tsconfig}`);
-	})
-	.then(() => {
-		fs.promises.readdir(distDir).then((filenames) => {
-			for (const filename of filenames) {
-				if (filename.endsWith('.test.js')) {
-					require(path.join(distDir, filename));
-				}
+(async () => {
+	await Promise.all([
+		fs.promises.rmdir(testDistDir, { recursive: true }),
+		fs.promises.rmdir(testNodeModulesDir, { recursive: true })
+	]);
+	await fs.promises.mkdir(testNodeModulesDir, { recursive: true });
+	await Promise.all([
+		exec(`tsc --project ${tsconfig}`),
+		fs.promises.symlink(path.join(rootDir, 'dist'), path.join(testNodeModulesDir, packageJson.name), 'dir')
+	]);
+	return fs.promises.readdir(testDistDir).then((filenames) => {
+		for (const filename of filenames) {
+			if (filename.endsWith('.test.js')) {
+				require(path.join(testDistDir, filename));
 			}
-		});
+		}
 	});
+})();
