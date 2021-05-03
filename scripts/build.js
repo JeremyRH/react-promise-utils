8a04e4cc29dcf740d62f828d71c1c80fa9e3d7d2
JeremyRH <holcomb227@gmail.com>
2021-05-03 13:51:33 -0700

    Add README to dist

--- scripts/build.js
+++ scripts/build.js
@@ -4,9 +4,15 @@ const { exec } = require('./exec');
 
 const distDir = path.resolve(__dirname, '../dist');
 const packageJson = path.resolve(__dirname, '../package.json');
+const readme = path.resolve(__dirname, '../README.md');
 const tsconfig = path.resolve(__dirname, '../tsconfig.json');
 
 fs.promises
 	.rmdir(distDir, { recursive: true })
 	.then(() => exec(`tsc --project ${tsconfig}`))
-	.then(() => fs.promises.copyFile(packageJson, path.join(distDir, 'package.json')));
+	.then(() =>
+		Promise.all([
+			fs.promises.copyFile(packageJson, path.join(distDir, 'package.json')),
+			fs.promises.copyFile(readme, path.join(distDir, 'README.md'))
+		])
+	);
