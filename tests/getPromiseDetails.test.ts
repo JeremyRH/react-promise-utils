1c97b09c2b51f75ffb7b25480f7796a10c8d757f
JeremyRH <holcomb227@gmail.com>
2021-05-03 00:02:45 -0700

    Add more getPromiseDetails tests

--- tests/getPromiseDetails.test.ts
+++ tests/getPromiseDetails.test.ts
@@ -1,4 +1,36 @@
 import { strict as assert } from 'assert';
-import { getPromiseDetails } from 'react-promise-utils/getPromiseDetails';
+import { getPromiseDetails, PromiseStatus } from 'react-promise-utils/getPromiseDetails';
 
 assert.equal(typeof getPromiseDetails, 'function');
+
+// Fulfilled promise.
+(async () => {
+	const willFulfill = Promise.resolve(1);
+	assert.deepEqual(getPromiseDetails(willFulfill), [PromiseStatus.PENDING, undefined]);
+	assert.equal(await willFulfill, 1);
+	assert.deepEqual(getPromiseDetails(willFulfill), [PromiseStatus.FULFILLED, 1]);
+})();
+
+// Rejected promise.
+(async () => {
+	const error = new Error('rejected promise test');
+	const willReject = Promise.reject(error);
+	assert.deepEqual(getPromiseDetails(willReject), [PromiseStatus.PENDING, undefined]);
+
+	let rejectedError;
+	try {
+		await willReject;
+	} catch (err) {
+		rejectedError = err;
+	}
+	assert.equal(rejectedError, error);
+
+	assert.deepEqual(getPromiseDetails(willReject), [PromiseStatus.REJECTED, error]);
+})();
+
+// Promises not seen before are always pending initially.
+(async () => {
+	const willFulfill = Promise.resolve(1);
+	await willFulfill;
+	assert.deepEqual(getPromiseDetails(willFulfill), [PromiseStatus.PENDING, undefined]);
+})();
